617c2781eff77693baf4434bf0cfa4bc36f57520
siddiquen <nashwas888@gmail.com>
2023-09-03 23:44:09 -0400

    continued to setup login

---
+++ src/Routes/index.jsx
@@ -0,0 +1,11 @@
+import {
+    createBrowserRouter
+} from "react-router-dom";
+import Login from "../pages/Login";
+
+export const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login />, //runs the Login script here
+    },
+  ]);
--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { LoginAPI } from "../api/AuthAPI";
+
 
 export default function LoginComponent() {
     return (
         <div>
             <h1>LoginComponent</h1>
+            <button >Log in to Linkedin</button>
         </div>
     );
 }
