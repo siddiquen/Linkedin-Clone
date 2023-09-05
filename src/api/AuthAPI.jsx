d9269ebe9fd95e5391edfe57b69f07ab02e13513
siddiquen <nashwas888@gmail.com>
2023-09-04 22:06:03 -0400

    attached auth to Login

--- src/api/AuthAPI.jsx
+++ src/api/AuthAPI.jsx
@@ -1,1 +1,7 @@
-export const LoginAPI = () => {};
+import { signInWithEmailAndPassword, getAuth } from 'firebase/auth'
+
+let auth = getAuth();
+
+export const LoginAPI = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password);
+};
--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { LoginAPI } from "../api/AuthAPI";
-
+import "../sass/LoginComponent.scss";
 
 export default function LoginComponent() {
+    //function that runs the loginAPI
+    //it will get used in the login button
+    const login = () => {
+        let res = LoginAPI();
+        console.log(res);
+    }
     return (
         <div>
             <h1>LoginComponent</h1>
-            <button >Log in to Linkedin</button>
+            
+            <button onClick={login} className='login-btn'>Log in to Linkedin</button>
         </div>
     );
 }
