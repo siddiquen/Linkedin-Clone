de789863d246f5efb3674a138c9d64d1d98e18c2
siddiquen <nashwas888@gmail.com>
2023-10-05 20:32:37 -0400

    set up loader as final step of auth guard

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Space, Spin } from 'antd';
-import './common.scss';
 
 export default function Loader() {
-    return
-    <div className='loader'>
-        <Space size="middle">
-            <Spin size="large" />
-        </Space>
-    </div>;
+    return (
+        <div className='loader'>
+            <p>Loading...Please Wait...</p>
+            <Space size="middle">
+                <Spin size="large" />
+            </Space>
+        </div>
+    );
 }
