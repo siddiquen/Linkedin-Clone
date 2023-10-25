7a6867a38b92dc8f97a357a179396753b1dbfcdd
siddiquen <nashwas888@gmail.com>
2023-10-24 23:00:39 -0400

    finalized navbar layout

--- src/components/common/TopBar/index.jsx
+++ src/components/common/TopBar/index.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import LinkedinLogo from '../../../assets/Linkedin_Logo.png';
+import userPic from '../../../assets/userPic.png';
 import './index.scss';
-import {AiOutlineHome} from 'react-icons/ai';
+import {
+    AiOutlineHome,
+    AiOutlineUserSwitch, 
+    AiOutlineSearch,
+    AiOutlineMessage,
+    AiOutlineBell} from 'react-icons/ai';
+import {BsBriefcase} from 'react-icons/bs';
+
 
 export default function TopBar() {
     return <div className='topbar-main'>
         <img className="linkedin-logo" src={LinkedinLogo} alt='LinkedinLogo'/>
-        <AiOutlineHome size={25}/>
+        <div className='react-icons'>
+            <AiOutlineSearch size={20} className='react-icon'/>
+            <AiOutlineHome size={20} className='react-icon'/>
+            <AiOutlineUserSwitch size={20} className='react-icon'/>
+            <BsBriefcase size={20} className='react-icon'/>
+            <AiOutlineMessage size={20} className='react-icon'/>
+            <AiOutlineBell size={20} className='react-icon'/>
+        </div>
+        <img className="userPic" src={userPic} alt='userPic'/>
     </div>;
     
 }
