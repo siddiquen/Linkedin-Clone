4d152d2c1e74d015b9a539adeff4ff76e1e5ef8f
siddiquen <nashwas888@gmail.com>
2023-10-30 23:20:23 -0400

    set up profile edit feature

--- src/components/ProfileComponent.jsx
+++ src/components/ProfileComponent.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import ProfileEdit from "./common/ProfileEdit";
 import ProfileCard from "./common/ProfileCard/index.jsx";
 
 export default function ProfileComponent({currentUser}) {
+    const [isEdit, setIsEdit] = useState(false);
+    const onEdit = () => {
+        setIsEdit(!isEdit);
+    };
     return (
         <div>
-            <ProfileCard currentUser = {currentUser} />
+            {isEdit ? (<ProfileEdit />) : (<ProfileCard currentUser = {currentUser} onEdit={onEdit}/>)}
         </div>
     );
 }
--- src/components/common/ProfileCard/index.jsx
+++ src/components/common/ProfileCard/index.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import ProfileEdit from "../ProfileEdit";
 import "./index.scss";
 
-export default function ProfileCard({currentUser}) {
-    return <div className="proffile-card"><h3>{currentUser.name}</h3></div>
+export default function ProfileCard({currentUser, onEdit}) {
+
+    return (
+        <>
+            {<div className="profile-card">
+                <div className="edit-btn">
+                    <button onClick={onEdit}>Edit</button>
+                </div>
+                <h3 className="userName">{currentUser.name}</h3>
+                <p className="userEmail">{currentUser.email}</p>
+            </div>}
+        </>
+    );
 }
