799c8950c5fb8dcc8bdd79077668de7674882655
siddiquen <nashwas888@gmail.com>
2023-11-06 21:00:49 -0500

    continued designing profile page

--- src/components/common/ProfileCard/index.jsx
+++ src/components/common/ProfileCard/index.jsx
@@ -3,6 +3,7 @@ import PostsCard from "../PostsCard";
 import { getSingleStatus, getSingleUser } from "../../../api/FirestoreAPIs";
 import { useLocation } from "react-router-dom";
 import "./index.scss";
+import { HiOutlinePencil} from "react-icons/hi";
 
 export default function ProfileCard({currentUser, onEdit}) {
     let location = useLocation();
@@ -19,13 +20,16 @@ export default function ProfileCard({currentUser, onEdit}) {
         <>
             <div className="profile-card">
                 <div className="edit-btn">
-                    <button onClick={onEdit}>Edit</button>
+                    <HiOutlinePencil className="edit-icon" onClick={onEdit}/>
                 </div>
                 <div className="profileInfo">
                     <div>
                         <h3 className="userName">{Object.values(currentProfile).length === 0 ? currentUser.name : currentProfile?.name}</h3>
                         <p className="heading">{Object.values(currentProfile).length === 0 ? currentUser.headline : currentProfile?.headline}</p>
-                        <p className="location">{Object.values(currentProfile).length === 0 ? currentUser.location : currentProfile?.location}</p>
+                        <p className="city">
+                            {Object.values(currentProfile).length === 0 ? `${currentUser.city}, ${currentUser.country}` : `${currentProfile?.city}, ${currentProfile?.country}`}
+                        </p>
+                        <a className="website" target = "_blank" href={Object.values(currentProfile).length === 0 ? currentUser.website : currentProfile?.website}>{Object.values(currentProfile).length === 0 ? currentUser.website : currentProfile?.website}</a>
                     </div>
 
                     <div className="rightInfo">
@@ -33,7 +37,12 @@ export default function ProfileCard({currentUser, onEdit}) {
                         <p className="university">{Object.values(currentProfile).length === 0 ? currentUser.university : currentProfile?.university}</p>
                     </div>
                 </div>
-                
+                <p className="about">{Object.values(currentProfile).length === 0 ? currentUser.about : currentProfile?.about}</p>
+                <p className="skills">
+                    <span className="skills-label">Skills:</span> &nbsp;
+                    {Object.values(currentProfile).length === 0 ? currentUser.skills : currentProfile?.skills}
+                </p>
+
             </div>
             <div className="post-status-main">
                 {allStatuses?.map((posts) => {
--- src/components/common/ProfileEdit/index.jsx
+++ src/components/common/ProfileEdit/index.jsx
@@ -3,7 +3,7 @@ import { editProfile} from "../../../api/FirestoreAPIs.jsx";
 import './index.scss';
 
 export default function ProfileEdit({onEdit, currentUser}) {
-    const [editInputs, setEditInputs] = useState({});
+    const [editInputs, setEditInputs] = useState(currentUser);
     const getInput = (event) => {
         let {name, value} = event.target;
         let input = {[name]: value};
@@ -20,11 +20,26 @@ export default function ProfileEdit({onEdit, currentUser}) {
             <button onClick={onEdit}>Go Back</button>
         </div>
         <div className='profileEditInputs'>
-            <input onChange={getInput} className='common-input' placeholder='Name' name='name'/>
-            <input onChange={getInput} className='common-input' placeholder='Headline' name='headline'/>
-            <input onChange={getInput} className='common-input' placeholder='Location' name='location'/>
-            <input onChange={getInput} className='common-input' placeholder='Company' name='company'/>
-            <input onChange={getInput} className='common-input' placeholder='University' name='university'/>
+            <label>Full Name</label>
+            <input onChange={getInput} className='common-input' placeholder='Name' name='name' value={editInputs.name}/>
+            <label>Headline</label>
+            <input onChange={getInput} className='common-input' placeholder='Headline' name='headline' value={editInputs.headline}/>
+            <label>Country</label>
+            <input onChange={getInput} className='common-input' placeholder='Country' name='country' value={editInputs.country}/>
+            <label>City</label>
+            <input onChange={getInput} className='common-input' placeholder='City' name='city' value={editInputs.city}/>
+            <label>Company</label>
+            <input onChange={getInput} className='common-input' placeholder='Company' name='company' value={editInputs.company}/>
+            <label>Industry</label>
+            <input onChange={getInput} className='common-input' placeholder='Industry' name='industry' value={editInputs.industry}/>
+            <label>University</label>
+            <input onChange={getInput} className='common-input' placeholder='University' name='university' value={editInputs.university}/>
+            <label>Website</label>
+            <input onChange={getInput} className='common-input' placeholder='Website' name='website' value={editInputs.website}/>
+            <label>About</label>
+            <textArea rows = {5} onChange={getInput} className='common-textarea' placeholder='About Me' name='about' value={editInputs.about}/>
+            <label>Skills</label>
+            <input onChange={getInput} className='common-input' placeholder='Skills' name='skills' value={editInputs.skills}/>
         </div>
         <div className="save-container">
             <button className="save-btn" onClick={updateProfileData}>Save</button>
